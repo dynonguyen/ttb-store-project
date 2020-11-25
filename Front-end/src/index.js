9332630d2b7e4266663d726e2d5873114dc89b81
Phan Xuan Bao <62206740+baroxfitxhcmus@users.noreply.github.com>
2020-11-25 18:00:01 +0700

    Merge branch component-product-view (#3)

--- Front-end/src/index.js
+++ Front-end/src/index.js
@@ -5,11 +5,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configStore from 'configs/configureStore';
 import App from 'containers/App';
+import ProductView from './components/ProductView/index';
 const store = configStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    {/* <App /> */}
+    <ProductView  item={{name:'Màn hình máy tính LG UltraWide™ 29inch IPS 75Hz AMD FreeSync™ HDR 29WN600-W'
+    ,price:200000,initialPrice:300000,remaining:'Còn 1 sản phẩm'}}/>
   </Provider>,
   document.getElementById('root'),
 );
+
