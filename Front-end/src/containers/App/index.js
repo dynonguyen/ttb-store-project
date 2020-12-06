f30050c29f12a78aa67624345c8958ed049ae9dd
= <leanhtuanlhln@gmail.com>
2020-12-06 23:59:24 +0700

    this is filter feature but it's not really completely

--- Front-end/src/containers/App/index.js
+++ Front-end/src/containers/App/index.js
@@ -1,33 +1,12 @@
-//commons css
-import 'commons/utils/index.scss';
-import 'antd/dist/antd.css';
-
-//configuration
-import 'configs/message.config';
-
 //React
-import React, { Suspense } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import routesConfig from 'configs/routesConfig';
-import GlobalLoading from 'components/Loading/Global';
-import ProductView from 'components/ProductView';
+import React from 'react';
+import Filter from 'components/Filter/index';
 
 function App() {
-  const { renderRoutes, routes } = routesConfig;
-  //rendering...
   return (
-    <BrowserRouter>
-      <Suspense fallback={<GlobalLoading />}>
-        <div className="App">
-          <Switch>
-            {renderRoutes(routes)}
-            <Route>
-              <h1>Not found</h1>
-            </Route>
-          </Switch>
-        </div>
-      </Suspense>
-    </BrowserRouter>
+    <div className="App">
+      <Filter />
+    </div>
   );
 }
 
